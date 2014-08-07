a0022d93b8143ae97eb946b06fb81f04a5deff3f
RoqueDeicide <roquegodslayer@hotmail.com>
2014-08-07 16:40:34 +0400

    Added some memory mapping utilities.

---
+++ CryBrary/Mathematics/MemoryMapping/Bytes8.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CryEngine.Mathematics.MemoryMapping
+{
+	/// <summary>
+	/// Encapsulates 8 bytes worth of data.
+	/// </summary>
+	[StructLayout(LayoutKind.Explicit, Size = 8)]
+	public unsafe struct Bytes8 : IBuffer
+	{
+		#region Fields
+		/// <summary>
+		/// <see cref="UInt64" /> value.
+		/// </summary>
+		[FieldOffset(0)]
+		public ulong UnsignedLong;
+		/// <summary>
+		/// <see cref="Int64" /> value.
+		/// </summary>
+		[FieldOffset(0)]
+		public long SignedLong;
+		/// <summary>
+		/// <see cref="Double" /> value.
+		/// </summary>
+		[FieldOffset(0)]
+		public double DoubleFloat;
+		/// <summary>
+		/// Individual 8 bytes.
+		/// </summary>
+		[FieldOffset(0)]
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+		public fixed byte Bytes[8];
+		#endregion
+		#region Properties
+		/// <summary>
+		/// Returns 8.
+		/// </summary>
+		public int Length
+		{
+			get { return 8; }
+		}
+		/// <summary>
+		/// Gets or sets a byte.
+		/// </summary>
+		/// <param name="index">Zero-based index of the byte to get or set.</param>
+		public byte this[int index]
+		{
+			get { return this.Bytes[index]; }
+			set { this.Bytes[index] = value; }
+		}
+		#endregion
+		#region Construction
+		/// <summary>
+		/// Initializes new instance of <see cref="Bytes8" /> type.
+		/// </summary>
+		/// <param name="value"><see cref="Int64" /> value to initialize this object with.</param>
+		public Bytes8(long value)
+			: this()
+		{
+			this.SignedLong = value;
+		}
+		/// <summary>
+		/// Initializes new instance of <see cref="Bytes8" /> type.
+		/// </summary>
+		/// <param name="value"><see cref="UInt64" /> value to initialize this object with.</param>
+		public Bytes8(ulong value)
+			: this()
+		{
+			this.UnsignedLong = value;
+		}
+		/// <summary>
+		/// Initializes new instance of <see cref="Bytes8" /> type.
+		/// </summary>
+		/// <param name="value"><see cref="Double" /> value to initialize this object with.</param>
+		public Bytes8(double value)
+			: this()
+		{
+			this.DoubleFloat = value;
+		}
+		/// <summary>
+		/// Initializes new 8-byte buffer object.
+		/// </summary>
+		/// <param name="array">Array that provides elements to fill new buffer with.</param>
+		/// <param name="startIndex">
+		/// Index of the first element of the array portion from which to start copying bytes.
+		/// </param>
+		/// <param name="count">Number of elements to copy from array.</param>
+		public Bytes8(byte[] array, int startIndex, int count)
+			: this()
+		{
+			if (array == null || array.Length == 0)
+			{
+				throw new ArgumentNullException
+					("array", "Array which elements are supposed to be transferred to buffer is null or empty.");
+			}
+			if (array.LongLength - startIndex < count)
+			{
+				throw new ArgumentOutOfRangeException
+					("count", "Too many elements are requested to be transferred to buffer.");
+			}
+			if (this.Length < count)
+			{
+				throw new ArgumentException("This buffer is too small.");
+			}
+			for (int i = 0, j = startIndex; i < count; i++, j++)
+			{
+				this.Bytes[i] = array[j];
+			}
+		}
+		/// <summary>
+		/// Initializes new instance of <see cref="Bytes8" /> type.
+		/// </summary>
+		/// <param name="pointer">Pointer to native memory cluster.</param>
+		/// <param name="index">
+		/// Zero-based index of the first of 8 bytes within native memory cluster.
+		/// </param>
+		public Bytes8(IntPtr pointer, int index)
+			: this()
+		{
+			if (pointer == null)
+			{
+				throw new ArgumentNullException("pointer", "Bytes8.Constructor: Unable to initialize new object of type Bytes8: Provided pointer is null.");
+			}
+			this.SignedLong = Marshal.ReadInt64(pointer, index);
+		}
+		/// <summary>
+		/// Initializes this buffer from a range of <see cref="Single" /> values.
+		/// </summary>
+		/// <param name="values">Array of arguments of type <see cref="Single" />.</param>
+		public Bytes8(params float[] values)
+			: this()
+		{
+			for (int i = 0; i < this.Length / 2 && i < values.Length; i++)
+			{
+				byte[] valueBytes = BitConverter.GetBytes(values[i]);
+				for (int j = 0; j < 4; j++)
+				{
+					this.Bytes[i * 4 + j] = valueBytes[j];
+				}
+			}
+		}
+		#endregion
+		#region Interface
+		/// <summary>
+		/// Gets 8 bytes from native memory cluster.
+		/// </summary>
+		/// <param name="handle">Pointer to the beginning of native memory cluster.</param>
+		/// <param name="offset">
+		/// Zero-based index of first of 8 bytes within native memory cluster to get.
+		/// </param>
+		public void Get(IntPtr handle, int offset)
+		{
+			this.SignedLong = Marshal.ReadInt64(handle, offset);
+		}
+		/// <summary>
+		/// Writes 8 bytes to native memory cluster.
+		/// </summary>
+		/// <param name="handle">Pointer to the beginning of native memory cluster.</param>
+		/// <param name="offset">
+		/// Zero-based index of first of 8 bytes within native memory cluster to set.
+		/// </param>
+		public void Set(IntPtr handle, int offset)
+		{
+			Marshal.WriteInt64(handle, offset, this.SignedLong);
+		}
+		#endregion
+	}
+}
---
+++ CryBrary/Mathematics/MemoryMapping/IBuffer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace CryEngine.Mathematics.MemoryMapping
+{
+	/// <summary>
+	/// Defines common properties of buffer structures.
+	/// </summary>
+	[ContractClass(typeof(IBufferContract))]
+	public interface IBuffer
+	{
+		/// <summary>
+		/// Gets length of the buffer in bytes.
+		/// </summary>
+		int Length { get; }
+		/// <summary>
+		/// When implemented in derived class or structure, gets or sets one byte in this buffer.
+		/// </summary>
+		/// <param name="index">Zero-based index of the byte to get or set.</param>
+		byte this[int index] { get; set; }
+		/// <summary>
+		/// When implemented in derived type, fills this buffer with data from native memory.
+		/// </summary>
+		/// <param name="handle">Address of the memory cluster from which to get the data.</param>
+		/// <param name="offset">
+		/// Zero-based index of the first byte to get from native memory within the cluster.
+		/// </param>
+		void Get(IntPtr handle, int offset);
+		/// <summary>
+		/// When implemented in derived type, transfers data from this buffer to native memory.
+		/// </summary>
+		/// <param name="handle">Address of the memory cluster to write the data into.</param>
+		/// <param name="offset">
+		/// Zero-based index of the first byte within the cluster to write.
+		/// </param>
+		void Set(IntPtr handle, int offset);
+	}
+	[ContractClassFor(typeof(IBuffer))]
+	internal abstract class IBufferContract : IBuffer
+	{
+		int IBuffer.Length
+		{
+			get
+			{
+				Contract.Ensures(Contract.Result<int>() > 0, "Length of the buffer must be greater then zero.");
+				return default(int);
+			}
+		}
+		byte IBuffer.this[int index]
+		{
+			get
+			{
+				Contract.Requires(index > -1, "Index of the byte to access must be greater or equal to zero.");
+				Contract.Requires(index < ((IBuffer)this).Length, "Index of the byte to access must be less then buffer length.");
+				return default(int);
+			}
+			// ReSharper disable ValueParameterNotUsed
+			set
+			// ReSharper restore ValueParameterNotUsed
+			{
+				Contract.Requires(index > -1, "Index of the byte to access must be greater or equal to zero.");
+				Contract.Requires(index < ((IBuffer)this).Length, "Index of the byte to access must be less then buffer length.");
+			}
+		}
+		void IBuffer.Get(IntPtr handle, int offset)
+		{
+			Contract.Requires(handle != IntPtr.Zero, "Pointer to memory block may not be null.");
+		}
+		void IBuffer.Set(IntPtr handle, int offset)
+		{
+			Contract.Requires(handle != IntPtr.Zero, "Pointer to memory block may not be null.");
+		}
+	}
+}
