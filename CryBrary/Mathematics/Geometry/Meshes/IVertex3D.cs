244f4a40e7fa23f68dc017b8c136cc0139b83b5b
RoqueDeicide <roquegodslayer@hotmail.com>
2014-08-25 10:33:01 +0400

    Implemented basic vertex and triangular face types.

---
+++ CryBrary/Mathematics/Geometry/Meshes/IVertex3D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryEngine.Mathematics.Geometry.Meshes
+{
+	/// <summary>
+	/// Defines common properties of vertices located in 3D space.
+	/// </summary>
+	public interface IVertex3D : IVertex
+	{
+		/// <summary>
+		/// When implemented, gets 3-dimensional vector that represents location of this vertex in 3D space.
+		/// </summary>
+		Vector3 Location { get; }
+	}
+}
