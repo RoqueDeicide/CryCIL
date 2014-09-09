aff00602d676d2cc68f4b6646ad8389f97b94a36
RoqueDeicide <roquegodslayer@hotmail.com>
2014-09-09 17:40:55 +0400

    Added stub for Actor attribute.

--- CryBrary/Attributes.cs
+++ CryBrary/Attributes.cs
@@ -143,4 +143,28 @@ namespace CryEngine
 		/// </summary>
 		public bool Default { get; set; }
 	}
+	/// <summary>
+	/// Applied to classes to mark them as actors.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+	public sealed class ActorAttribute : Attribute
+	{
+		// See the attribute guidelines at 
+		//  http://go.microsoft.com/fwlink/?LinkId=85236
+		private readonly string positionalString;
+
+		// This is a positional argument
+		public ActorAttribute(string positionalString)
+		{
+			this.positionalString = positionalString;
+
+			// TODO: Implement code here
+			throw new NotImplementedException();
+		}
+
+		public string PositionalString { get; private set; }
+
+		// This is a named argument
+		public int NamedInt { get; set; }
+	}
 }
