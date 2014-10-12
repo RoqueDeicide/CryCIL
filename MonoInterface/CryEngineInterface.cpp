989e2fec9ec6bccc6cadee41c1f993cbd434ae1a
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-12 20:39:28 +0400

    Added file for functions that are exported.
    Added some simple test functions.

---
+++ MonoInterface/CryEngineInterface.cpp
@@ -0,0 +1,22 @@
+// All functions that are exported to be used by CryEngine Game Dll.
+
+#include "stdafx.h"
+#include "CryEngineHeaders.h"
+
+extern "C"	// Mark exported functions as C code, so the compiler keeps function names as they are.
+{
+	MONOINTERFACE_API void Test()
+	{
+		if (gEnv)
+		{
+			gEnv->pLog->LogAlways("Hello World!");
+		}
+	}
+	MONOINTERFACE_API void TestText(const char* arg)
+	{
+		if (gEnv)
+		{
+			gEnv->pLog->LogAlways(arg);
+		}
+	}
+}
