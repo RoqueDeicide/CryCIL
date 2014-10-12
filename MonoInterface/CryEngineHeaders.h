92b58c2948cd790abf17c3f7f9b65b2eefcb631f
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-12 20:38:50 +0400

    Added header file for CryEngine header includes.

---
+++ MonoInterface/CryEngineHeaders.h
@@ -0,0 +1,11 @@
+
+#include <CryThread.h>
+#include <Cry_Math.h>
+#include <ISystem.h>
+#include <I3DEngine.h>
+#include <IInput.h>
+#include <IConsole.h>
+#include <ITimer.h>
+#include <ILog.h>
+#include <IGameplayRecorder.h>
+#include <ISerialize.h>
