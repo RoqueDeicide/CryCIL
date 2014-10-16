4311e727a2e60d6976d1e9917776128e6beef564
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-16 04:58:54 +0400

    Added stub for class provides access to CryEngine logging functionality.

---
+++ Cryambly/Engine/DebugServices/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryCil.Engine.DebugServices
+{
+	/// <summary>
+	/// Represents an interface between CryCIL and CryEngine debug log functionality.
+	/// </summary>
+	public static class Log
+	{
+		/// <summary>
+		/// Write a line into the log.
+		/// </summary>
+		/// <param name="value">Text to write.</param>
+		public static void Line(string value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
