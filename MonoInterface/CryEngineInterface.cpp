d6efc2251821b26b490d381fe2fff90402a06322
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-17 09:36:46 +0400

    Now MonoInterface.dll actually works.

--- MonoInterface/CryEngineInterface.cpp
+++ MonoInterface/CryEngineInterface.cpp
@@ -4,18 +4,15 @@
 
 extern "C"	// Mark exported functions as C code, so the compiler keeps function names as they are.
 {
-	MONOINTERFACE_API void Test()
+	//! Invoked from Game dll to initialize this module.
+	//!
+	//! @remarks This function must be called before anything can be done with this module.
+	//!
+	//! @param system Pointer to ISystem object that will provide us with pointer to gEnv.
+	MONOINTERFACE_API void InitializeModule(ISystem *system)
 	{
-		if (gEnv)
-		{
-			gEnv->pLog->LogAlways("Hello World!");
-		}
-	}
-	MONOINTERFACE_API void TestText(const char* arg)
-	{
-		if (gEnv)
-		{
-			gEnv->pLog->LogAlways(arg);
-		}
+		// Initializes gEnv variable, registers some objects.
+		// Fun fact: Module name is only used for Unit Tests.
+		ModuleInitISystem(system, "MonoInterface");
 	}
 }
