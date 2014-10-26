474507543d03721dbb3308b28059f1b71e5a2954
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-26 15:11:01 +0400

    Imported EventArgs<T> from CryBrary.

---
+++ Cryambly/GenericEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CryCil
+{
+	/// <summary>
+	/// Represents an object that contains a parameter of type <see cref="ExtraDataType"/>
+	/// that describes an event.
+	/// </summary>
+	/// <typeparam name="ExtraDataType">Type of the parameter.</typeparam>
+	public class EventArgs<ExtraDataType> : EventArgs
+	{
+		/// <summary>
+		/// Extra parameter that comes with the event.
+		/// </summary>
+		public ExtraDataType Parameter { get; private set; }
+		/// <summary>
+		/// Creates a new instance of this type.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public EventArgs(ExtraDataType parameter)
+		{
+			this.Parameter = parameter;
+		}
+	}
+}
