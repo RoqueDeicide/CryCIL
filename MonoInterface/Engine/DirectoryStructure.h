17c8e73e4e7937ae58548b96f6a917b615371e8e
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-29 16:11:06 +0400

    Fixed DirectoryStructure in MonoInterface.dll

--- MonoInterface/Engine/DirectoryStructure.h
+++ MonoInterface/Engine/DirectoryStructure.h
@@ -4,16 +4,16 @@
 
 #include <sstream>
 
-#ifdef _WINDOWS
-#define PATH_SEPARATOR "\\"
-#else
+#if defined(PS3) || defined(LINUX) || defined(APPLE) || defined(ORBIS)
 #define PATH_SEPARATOR "/"
+#else
+#define PATH_SEPARATOR "\\"
 #endif // _WINDOWS
 
 #ifdef WIN32
-#define BINARIES_FOLDER          "Bin32"
+#define BINARIES_FOLDER         "Bin32"
 #else
-#define BINARIES_FOLDER          "Bin64"
+#define BINARIES_FOLDER         "Bin64"
 #endif // WIN32
 
 #define MODULES_FOLDER          "Modules"
@@ -23,92 +23,51 @@
 #define MONO_CONFIG_FOLDER      "etc"
 #define MONO_DEBUG_UTILITY_FILE "pdb2mdb.dll"
 
+//! Determines whether given text starts with given prefix.
+//!
+//! @param text   Text to check for prefix.
+//! @param prefix Prefix which presence to check at the start of the text.
+inline bool StartsWith(const char *text, const char *prefix)
+{
+	int textLength = strlen(text);
+	int prefixLength = strlen(prefix);
+
+	return textLength >= prefixLength && strncmp(text, prefix, prefixLength) == 0;
+}
+//! Determines whether given text ends with given postfix.
+//!
+//! @param text    Text to check for postfix.
+//! @param postfix postfix which presence to check at the end of the text.
+inline bool EndsWith(const char *text, const char *postfix)
+{
+	int textLength = strlen(text);
+	int postfixLength = strlen(postfix);
+
+	return textLength >= postfixLength &&
+		strncmp(text + textLength - postfixLength, postfix, postfixLength) == 0;
+}
+
 //! Defines some simple utilities for working with paths.
 struct PathUtilities
 {
-	//! Combines a bunch of folder and file names into a path.
-	//! @remark Don't put any path separators into parts.
+	//! Constructs a path from a collection of names.
 	//!
-	//! @param parts Pointer to an array of parts to combine.
-	//! @param count Number of parts in the array.
-	static char *Combine(const char** parts, int count)
-	{
-		std::stringstream resultantPath;
-
-		for (int i = 0; i < count; i++)
-		{
-			resultantPath << parts[i] << PATH_SEPARATOR;
-		}
-
-		return const_cast<char *>(resultantPath.str().c_str());
-	}
+	//! @param parts An array of names to combine.
+	static const char *Combine(std::vector<const char *> parts);
 };
 //! Defines functions that return paths to various folders within CryEngine installation.
 struct DirectoryStructure
 {
 	//! Returns a path to the folder that contains Mono configuration files.
-	static char *GetMonoConfigurationFolder()
-	{
-		char* parts[5];
-		parts[0] = BINARIES_FOLDER;
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		parts[3] = MONO_FOLDER;
-		parts[4] = MONO_CONFIG_FOLDER;
-		return PathUtilities::Combine((const char **)&parts[0], 5);
-	}
+	static const char *GetMonoConfigurationFolder();
 	//! Returns a path to the folder that contains Mono libraries.
-	static char *GetMonoLibraryFolder()
-	{
-		char* parts[5];
-		parts[0] = BINARIES_FOLDER;
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		parts[3] = MONO_FOLDER;
-		parts[4] = MONO_LIBS_FOLDER;
-		return PathUtilities::Combine((const char **)&parts[0], 5);
-	}
+	static const char *GetMonoLibraryFolder();
 	//! Returns a path to the folder that contains CryCIL libraries.
-	static char *GetMonoBinariesFolder()
-	{
-		char* parts[3];
-		parts[0] = BINARIES_FOLDER;
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		return PathUtilities::Combine((const char **)&parts[0], 3);
-	}
+	static const char *GetMonoBinariesFolder();
 	//! Returns a path to the file that contains Cryambly.
-	static char *GetCryamblyFile()
-	{
-		char* parts[4];
-		parts[0] = BINARIES_FOLDER;
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		parts[3] = "Cryambly.dll";
-		return PathUtilities::Combine((const char **)&parts[0], 4);
-	}
+	static const char *GetCryamblyFile();
 	//! Returns a path to the file that contains Pdb to Mdb converter.
-	static char *GetPdb2MdbFile()
-	{
-		char* parts[4];
-		parts[0] = "Bin32";
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		parts[3] = MONO_DEBUG_UTILITY_FILE;
-		return PathUtilities::Combine((const char **)&parts[0], 4);
-	}
+	static const char *GetPdb2MdbFile();
 	//! Returns a path to the file that contains configuration data for 4.5 version AppDomains.
-	static char *GetMonoAppDomainConfigurationFile()
-	{
-		char* parts[8];
-		parts[0] = BINARIES_FOLDER;
-		parts[1] = MODULES_FOLDER;
-		parts[2] = CRYCIL_FOLDER;
-		parts[3] = MONO_FOLDER;
-		parts[4] = MONO_CONFIG_FOLDER;
-		parts[5] = "mono";
-		parts[6] = "4.5";
-		parts[7] = "machine.config";
-		return PathUtilities::Combine((const char **)&parts[0], 8);
-	}
+	static const char *GetMonoAppDomainConfigurationFile();
 };
