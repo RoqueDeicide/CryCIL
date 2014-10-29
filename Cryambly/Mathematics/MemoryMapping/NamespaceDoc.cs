f9c295a08887b3569a0a059d1d183a295326d0f1
RoqueDeicide <roquegodslayer@hotmail.com>
2014-10-30 01:15:45 +0400

    Documented Mathematics namespaces.

---
+++ Cryambly/Mathematics/MemoryMapping/NamespaceDoc.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CryCil.Mathematics.MemoryMapping
+{
+	/// <summary>
+	/// Contains types that are used for memory reinterpretation.
+	/// </summary>
+	[CompilerGenerated]
+	public class NamespaceDoc
+	{
+	}
+}
