1102db5db00cdfeda434f3e19f2b19014bf5993b
RoqueDeicide <roquegodslayer@hotmail.com>
2014-11-10 05:09:32 +0400

    Added class that allows to temporarily switch log post type on redirected console output.

---
+++ Cryambly/RunTime/Logging/ConsoleOutputLevel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryCil.Engine.DebugServices;
+
+namespace CryCil.RunTime.Logging
+{
+	/// <summary>
+	/// Represents an object that can be used to override custom console output importance level.
+	/// </summary>
+	public class ConsoleOutputLevel : ITemporaryOverrider
+	{
+		private readonly LogPostType originalPostType;
+		/// <summary>
+		/// Creates an object that temporarily switches console output level to the different one.
+		/// </summary>
+		/// <param name="tempPostLevel">New console output level.</param>
+		public ConsoleOutputLevel(LogPostType tempPostLevel)
+		{
+			this.originalPostType = ConsoleLogWriter.PostType;
+			ConsoleLogWriter.PostType = tempPostLevel;
+		}
+		/// <summary>
+		/// Resets the previous console output level.
+		/// </summary>
+		public void Dispose()
+		{
+			ConsoleLogWriter.PostType = this.originalPostType;
+		}
+	}
+}
