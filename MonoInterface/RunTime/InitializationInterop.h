596d73ff455ca7d19d48c3a602f4fa236af7d51d
RoqueDeicide <roquegodslayer@hotmail.com>
2014-12-19 20:58:52 +0400

    InitializationInterop now deletes itself on post-initialization.

--- MonoInterface/RunTime/InitializationInterop.h
+++ MonoInterface/RunTime/InitializationInterop.h
@@ -10,6 +10,11 @@ struct InitializationInterop : public IDefaultMonoInterop
 	virtual void OnRunTimeInitialized();
 
 	virtual void SetInterface(IMonoInterface *handle);
+	virtual void OnPostInitialization()
+	{
+		MonoEnv->RemoveListener(this);
+		delete this;
+	}
 
 	static void OnCompilationStartingBind();
 	static void OnCompilationCompleteBind(bool success);
