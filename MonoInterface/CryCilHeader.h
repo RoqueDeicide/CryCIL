898980b2eeec1f39823f1290048a38406f06ef8a
RoqueDeicide <roquegodslayer@hotmail.com>
2014-12-19 20:41:45 +0400

    Added an option to initialize MonoEnv before its initialization is complete.

--- MonoInterface/CryCilHeader.h
+++ MonoInterface/CryCilHeader.h
@@ -7,6 +7,43 @@
 #include "IMonoInterface.h"
 // Define the external variable that was declared in IMonoInterface.h
 IMonoInterface *MonoEnv = nullptr;
+//! Represents an event listener that initializes MonoEnv, when needed.
+struct EarlyInitializer : IMonoSystemListener
+{
+	//! Sets MonoEnv and removes itself.
+	virtual void SetInterface(IMonoInterface *handle)
+	{
+		MonoEnv = handle;
+		MonoEnv->RemoveListener(this);
+		delete this;
+	}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnPreInitialization() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnRunTimeInitializing() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnRunTimeInitialized() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnCryamblyInitilizing() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnCompilationStarting() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnCompilationComplete(bool success) {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual List<int> *GetSubscribedStages() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnInitializationStage(int stageIndex) {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnCryamblyInitilized() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void OnPostInitialization() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void Update() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void PostUpdate() {}
+	//! Not used since this listener is set to unregister itself after MonoEnv is set.
+	virtual void Shutdown() {}
+};
 //! Loads up and initializes CryCIL.
 //!
 //! Invoke this function before using CryCil and after CryAction.dll is loaded
@@ -17,12 +54,19 @@ IMonoInterface *MonoEnv = nullptr;
 //!
 //! Attempting to free library represented by the returned handle is likely to cause BEX.
 //!
-//! @param framework Pointer to implementation of IGameFramework.
-//! @param listeners Pointer to the array of persistent listeners to use.
-//!                  Can be null, if you don't want to register any.
+//! @param framework        Pointer to implementation of IGameFramework.
+//! @param listeners        Pointer to the array of persistent listeners to use.
+//!                         Can be null, if you don't want to register any.
+//! @param earlyMonoEnvInit Indicates whether MonoEnv should be set to the appropriate value
+//!                         before initialization is complete.
 //!
 //! @return A Dll handle that represents CryCil library.
-HMODULE InitializeCryCIL(IGameFramework *framework, List<IMonoSystemListener *> *listeners)
+HMODULE InitializeCryCIL
+(
+	IGameFramework *framework,
+	List<IMonoSystemListener *> *listeners,
+	bool earlyMonoEnvInit = true
+)
 {
 	HMODULE monoInterfaceDll = CryLoadLibrary(MONOINTERFACE_LIBRARY);
 	if (!monoInterfaceDll)
@@ -37,6 +81,14 @@ HMODULE InitializeCryCIL(IGameFramework *framework, List<IMonoSystemListener *> 
 		CryFatalError("Could not locate %s function within %s.", MONO_INTERFACE_INIT, MONOINTERFACE_LIBRARY);
 	}
 	CryLogAlways("Acquired a pointer to initializer function.");
+	if (earlyMonoEnvInit)
+	{
+		if (!listeners)
+		{
+			listeners = new List<IMonoSystemListener *>(1);
+		}
+		listeners->Add(new EarlyInitializer());
+	}
 	MonoEnv = initFunc(framework, listeners);
 	return monoInterfaceDll;
 }
