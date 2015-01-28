dc32d044b45ddde30084f46154beb9a801ddbd78
RoqueDeicide <roquegodslayer@hotmail.com>
2015-01-28 23:58:47 +0400

    Improved naming convention of API implementation files.

--- MonoInterface/Wrappers/MonoClass.cpp
+++ MonoInterface/Wrappers/MonoClass.cpp
@@ -371,3 +371,24 @@ IMonoClass * MonoClassWrapper::GetNestedType(const char *name)
 	}
 	return nullptr;
 }
+
+List<MonoClassWrapper *> MonoClassCache::cachedClasses(50);
+
+
+IMonoClass *MonoClassCache::Wrap(MonoClass *klass)
+{
+	// Cool lambda expression.
+	auto condition = [&klass](MonoClassWrapper *w)
+	{
+		return w->GetWrappedPointer() == klass;
+	};
+	if (MonoClassWrapper *wrapper = cachedClasses.Find(condition))
+	{
+		// Return registered wrapper.
+		return wrapper;
+	}
+	// Register a new one.
+	MonoClassWrapper *wrapper = new MonoClassWrapper(klass);
+	MonoClassCache::cachedClasses.Add(wrapper);
+	return wrapper;
+}
