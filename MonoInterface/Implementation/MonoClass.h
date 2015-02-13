ae7c73380aa9c7d6adaed390684ef88e30af3755
RoqueDeicide <roquegodslayer@hotmail.com>
2015-02-13 19:24:17 +0400

    Reshuffled IMonoClass implementation.

--- MonoInterface/Implementation/MonoClass.h
+++ MonoInterface/Implementation/MonoClass.h
@@ -28,55 +28,52 @@ public:
 	~MonoClassWrapper();
 
 	virtual IMonoConstructor *GetConstructor(IMonoArray *types = nullptr);
-
 	virtual IMonoConstructor *GetConstructor(List<IMonoClass *> &classes);
-
 	virtual IMonoConstructor *GetConstructor(List<Pair<IMonoClass *, const char *>> &specifiedClasses);
-
 	virtual IMonoConstructor *GetConstructor(const char *params);
-
 	virtual IMonoConstructor *GetConstructor(List<const char *> &paramTypeNames);
-
 	virtual IMonoConstructor *GetConstructor(const char *name, int paramCount);
-
+	
 	virtual IMonoMethod *GetMethod(const char *name, IMonoArray *types = nullptr);
-
 	virtual IMonoMethod *GetMethod(const char *name, List<IMonoClass *> &classes);
-
 	virtual IMonoMethod *GetMethod(const char *name, List<Pair<IMonoClass *, const char *>> &specifiedClasses);
-
 	virtual IMonoMethod *GetMethod(const char *name, List<const char *> &paramTypeNames);
-	IMonoMethod *GetMethod(const char *name, const char *params);
-	IMonoMethod *GetMethod(const char *name, int paramCount);
-	virtual mono::type GetType();
+	virtual IMonoMethod *GetMethod(const char *name, const char *params);
+	virtual IMonoMethod *GetMethod(const char *name, int paramCount);
 
-	virtual mono::type MakeArrayType();
-
-	virtual mono::type MakeArrayType(int rank);
-
-	virtual mono::type MakeByRefType();
-
-	virtual mono::type MakePointerType();
 	virtual IMonoMethod **GetMethods(const char *name, int paramCount, int &foundCount);
 	virtual IMonoMethod **GetMethods(const char *name, int &foundCount);
-
+	
+	virtual mono::type GetType();
+	virtual mono::type MakeArrayType();
+	virtual mono::type MakeArrayType(int rank);
+	virtual mono::type MakeByRefType();
+	virtual mono::type MakePointerType();
+	
 	virtual void GetField(mono::object obj, const char *name, void *value);
 	virtual void SetField(mono::object obj, const char *name, void *value);
 	virtual void GetField(mono::object obj, IMonoField *field, void *value);
 	virtual void SetField(mono::object obj, IMonoField *field, void *value);
+	
 	virtual IMonoProperty *GetProperty(const char *name);
 	virtual IMonoEvent *GetEvent(const char *name);
+	
 	virtual bool Inherits(const char *nameSpace, const char *className);
+	virtual bool Implements(const char *nameSpace, const char *interfaceName, bool searchBaseClasses);
+	
 	virtual mono::object Box(void *value);
+	
 	virtual const char *GetName();
 	virtual const char *GetNameSpace();
 	virtual const char *GetFullName();
-	virtual IMonoAssembly *GetAssembly();
-	virtual void *GetWrappedPointer();
-	virtual bool Implements(const char *nameSpace, const char *interfaceName, bool searchBaseClasses);
-	virtual IMonoClass *GetBase();
-	virtual IMonoClass *GetNestedType(const char *name);
 	virtual const char *GetFullNameIL();
+	
+	virtual void *GetWrappedPointer();
+
+	virtual IMonoAssembly *GetAssembly();
+	virtual IMonoClass    *GetBase();
+	virtual IMonoClass    *GetNestedType(const char *name);
+	
 	virtual ReadOnlyList<IMonoField *>    *GetFields();
 	virtual ReadOnlyList<IMonoProperty *> *GetProperties();
 	virtual ReadOnlyList<IMonoEvent *>    *GetEvents();
@@ -84,7 +81,6 @@ public:
 private:
 	void GetFieldValue(mono::object obj, MonoClassField *field, void *value);
 	void SetFieldValue(mono::object obj, MonoClassField *field, void *value);
-
 };
 
 //! Caches MonoClassWrapper objects.
