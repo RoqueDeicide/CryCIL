17f9af683c3abb4ac58d7db9506ac6884c5f71e9
RoqueDeicide <roquegodslayer@hotmail.com>
2015-02-23 11:27:43 +0400

    Added test code for printing out members of the class.

--- MonoInterface/Testing/TestClasses.h
+++ MonoInterface/Testing/TestClasses.h
@@ -2,17 +2,49 @@
 
 #include "IMonoInterface.h"
 
+void TestMemberLists(IMonoClass *);
+
 void TestClasses()
 {
 	IMonoClass *vector3Class = MonoEnv->Cryambly->Vector3;
 
 	CryLogAlways("TEST: Checking %s.", vector3Class->FullName);
 
-	CryLogAlways("TEST: Printing a list of fields.");
+	TestMemberLists(vector3Class);
 
-	auto vector3Fields = vector3Class->Fields;
 
-	for (int i = 0; i < vector3Fields->Length; i++)
+
+void TestMemberLists(IMonoClass *klass)
+{
+
+	CryLogAlways("TEST: Printing a list of fields of type %s.", klass->Name);
+
+	auto fields = klass->Fields;
+
+	for (int i = 0; i < fields->Length; i++)
+	{
+		CryLogAlways("TEST: Field #%d: %s", i, fields->At(i)->Name);
+	}
+
+	CryLogAlways("TEST: Printing a list of properties of type %s.", klass->Name);
+
+	auto properties = klass->Properties;
+
+	for (int i = 0; i < properties->Length; i++)
+	{
+		CryLogAlways("TEST: Property #%d: %s", i, properties->At(i)->Name);
+	}
+
+	CryLogAlways("TEST: Printing a list of methods of type %s.", klass->Name);
+
+	auto methods = klass->Methods;
+
+	for (int i = 0; i < methods->Length; i++)
+	{
+		CryLogAlways("TEST: Method #%d: %s", i, methods->At(i)->Name);
+	}
+}
+
 	{
 		CryLogAlways("TEST: Field #%d: %s", i, vector3Fields->At(i)->Name);
 	}
