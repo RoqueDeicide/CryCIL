72734ea0da9104e5f03b967129cd6b3656c66177
RoqueDeicide <roquegodslayer@hotmail.com>
2015-03-07 07:40:37 +0400

    Added code for testing IMonoHandle implementation.

--- MonoInterface/Testing/TestObjects.h
+++ MonoInterface/Testing/TestObjects.h
@@ -2,5 +2,88 @@
 
 void TestObjects()
 {
+	CryLogAlways("TEST: Testing IMonoHandle implementation.");
 
+	IMonoClass *testObjectClass = mainTestingAssembly->GetClass("Test", "TestObject");
+
+	CryLogAlways("TEST: Creating an object.");
+
+	double number = 34.567;
+	void *param = &number;
+	mono::object testObj = testObjectClass->GetConstructor(1)->Create(&param);
+	IMonoHandle *obj = MonoEnv->Objects->Wrap(testObj);
+
+	IMonoGCHandle *handle = MonoEnv->GC->Keep(obj->Get());
+
+	CryLogAlways("TEST: Testing object's fields.");
+
+	int fieldNumber = 0;
+	obj->GetField("Number", &fieldNumber);
+
+	CryLogAlways("TEST: The integer field's value: %d", fieldNumber);
+
+	mono::string fieldText;
+	obj->GetField("Text", &fieldText);
+
+	CryLogAlways("TEST: The text field's value: %s", NtText(fieldText));
+
+	CryLogAlways("TEST: Testing object's property.");
+
+	auto prop = obj->GetProperty("DecimalNumber");
+
+	if (prop)
+	{
+		CryLogAlways("TEST SUCCESS: Successfully got a wrapper for a property %s.", prop->Name);
+	}
+	else
+	{
+		ReportError("TEST FAILURE: Unable to get the wrapper for a property DecimalNumber.");
+	}
+
+	CryLogAlways("TEST: Testing object's events.");
+
+	auto _event = obj->GetEvent("Something");
+
+	if (_event)
+	{
+		CryLogAlways("TEST SUCCESS: Successfully got a wrapper for an event %s.", _event->Name);
+	}
+	else
+	{
+		ReportError("TEST FAILURE: Unable to get the wrapper for an event Something.");
+	}
+
+	IMonoClass *declaringClass = obj->GetClass();
+
+	if (declaringClass)
+	{
+		CryLogAlways("TEST SUCCESS: Successfully got a wrapper for the object's class %s.", declaringClass->Name);
+	}
+	else
+	{
+		ReportError("TEST FAILURE: Unable to get the wrapper for the object's class.");
+	}
+
+	CryLogAlways("TEST: Testing updating the reference to the wrapped object after triggering GC.");
+
+	MonoEnv->GC->Collect();
+
+	obj->Update(handle->Object);
+
+	if (obj->Get())
+	{
+		CryLogAlways("TEST SUCCESS: Successfully got a reference to the object's new location.");
+	}
+	else
+	{
+		ReportError("TEST FAILURE: Unable to get the reference to the object's new location.");
+	}
+
+	CryLogAlways("TEST: Testing getting the field value after getting a new reference to the object.");
+
+	obj->GetField("Number", &fieldNumber);
+
+	CryLogAlways("TEST: The integer field's value: %d", fieldNumber);
+
+	handle->Release();
 }
---
+++ TestAssemblies/MainTestingAssembly/TestObject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainTestingAssembly
+{
+	/// <summary>
+	/// Represents an object that is used by underlying framework to test IMonoHandle implementation.
+	/// </summary>
+	public class TestObject
+	{
+		#region Fields
+		/// <summary>
+		/// An integer field.
+		/// </summary>
+		public int Number;
+		/// <summary>
+		/// A text field.
+		/// </summary>
+		public string Text;
+		#endregion
+		#region Properties
+		/// <summary>
+		/// A decimal number property.
+		/// </summary>
+		public double DecimalNumber { get; set; }
+		#endregion
+		#region Events
+		/// <summary>
+		/// A simple event.
+		/// </summary>
+		public event EventHandler Something;
+		#endregion
+		#region Construction
+		/// <summary>
+		/// A constructor for the object.
+		/// </summary>
+		/// <param name="someNumber">A number to assign to the object.</param>
+		public TestObject(double someNumber)
+		{
+			this.ChangeNumber(someNumber);
+		}
+		#endregion
+		#region Interface
+		/// <summary>
+		/// Prints the number to the console.
+		/// </summary>
+		public void Print()
+		{
+			Console.WriteLine("TEST: A number is {0}", this.Text);
+		}
+		/// <summary>
+		/// Changes the number this object works with.
+		/// </summary>
+		/// <param name="number">A new number.</param>
+		public void ChangeNumber(double number)
+		{
+			this.Number = Convert.ToInt32(number);
+			this.DecimalNumber = number;
+			this.Text = number.ToString("F", CultureInfo.CurrentUICulture);
+			this.Something +=
+				(sender, args) =>
+					Console.WriteLine("TEST: An event handler for an object that was created with a number {0:F} has been invoked.",
+									  this.DecimalNumber);
+		}
+		#endregion
+		#region Utilities
+		protected virtual void OnSomething()
+		{
+			EventHandler handler = this.Something;
+			if (handler != null) handler(this, EventArgs.Empty);
+		}
+		#endregion
+	}
+}
