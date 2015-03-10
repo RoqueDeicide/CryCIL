17f93aaeb10ff3f446d2566e83505881ac0e6190
RoqueDeicide <roquegodslayer@hotmail.com>
2015-03-10 19:29:42 +0400

    IMonoDelegate now caches IMonoFunction wrapper and automatically deletes it.

--- MonoInterface/Interfaces/IMonoDelegate.h
+++ MonoInterface/Interfaces/IMonoDelegate.h
@@ -5,15 +5,40 @@
 //! Wraps an object that represents a Mono delegate.
 struct IMonoDelegate : public IMonoObject
 {
+private:
+	IMonoFunction *func;
+public:
 	//! Creates new wrapper for given delegate.
 	IMonoDelegate(mono::delegat d)
 		: IMonoObject(d)
+		, func(nullptr)
 	{
 	}
 	//! Creates new wrapper for given delegate.
 	IMonoDelegate(MonoGCHandle &handle)
 		: IMonoObject(handle)
+		, func(nullptr)
 	{
+	}
+	~IMonoDelegate()
+	{
+		if (this->func)
+		{
+			delete this->func;
+		}
+	}
+	//! This operator does the same thing as one for base class, but it also releases cached IMonoFunction
+	//! wrapper.
+	IMonoDelegate &operator=(mono::delegat del)
+	{
+		if (this->obj != del)
+		{
+			if (this->func)
+			{
+				delete this->func;
+			}
+			this->obj = del;
+		}
 	}
 	//! Gets a wrapper for a Mono function that will be invoked by this delegate.
 	__declspec(property(get = GetFunction)) IMonoFunction *Function;
@@ -21,19 +46,23 @@ struct IMonoDelegate : public IMonoObject
 	__declspec(property(get = GetTarget)) mono::object Target;
 	//! Gets a raw function pointer that can be used to invoke this delegate.
 	//!
-	//! Returned function pointer may cease to exist after the delegate is GCed.
-	__declspec(property(get = GetFunctionPointer)) void *FunctionPointer;
+	//! Returned function pointer ceases to exist after the delegate is GCed.
+	__declspec(property(get = GetTrampoline)) void *Trampoline;
 
 	IMonoFunction *GetFunction()
 	{
-		return MonoEnv->Objects->GetDelegateFunction(this->obj);
+		if (!this->func)
+		{
+			this->func = MonoEnv->Objects->GetDelegateFunction(this->obj);
+		}
+		return this->func;
 	}
 	mono::object GetTarget()
 	{
 		return MonoEnv->Objects->GetDelegateTarget(this->obj);
 	}
-	void *GetFunctionPointer()
+	void *GetTrampoline()
 	{
-		return MonoEnv->Objects->GetDelegateFunctionPointer(this->obj);
+		return MonoEnv->Objects->GetDelegateTrampoline(this->obj);
 	}
 };
