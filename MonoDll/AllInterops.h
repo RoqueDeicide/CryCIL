bb0ac9da019e9db4e8b8394ba8d7a434dae00f94
RoqueDeicide <roquegodslayer@hotmail.com>
2015-04-10 18:00:33 +0400

    Another purge.

--- MonoDll/AllInterops.h
+++ MonoDll/AllInterops.h
@@ -12,23 +12,14 @@
 // Logic.
 #include "Interops/GameLogic/ActorSystem.h"
 #include "Interops/GameLogic/GameRules.h"
-// Native memory.
-#include "Interops/NativeMemory/CryMarshal.h"
 // Static objects and meshes.
 #include "Interops/StaticObjects/CMesh.h"
 #include "Interops/StaticObjects/StaticObject.h"
-// Utilities.
-#include "Interops/Utilities/Console.h"
-#include "Interops/Utilities/CryPak.h"
-#include "Interops/Utilities/Debug.h"
-#include "Interops/Utilities/Time.h"
 // Rendering.
 #include "Interops/Viewport/Renderer.h"
 #include "Interops/Viewport/ViewSystem.h"
 // Miscellaneous
 #include "Interops/CrySerialize.h"
 #include "Interops/LevelSystem.h"
-#include "Interops/MonoInput.h"
 #include "Interops/Network.h"
 #include "Interops/Platform.h"
-#include "Interops/ScriptTable.h"
