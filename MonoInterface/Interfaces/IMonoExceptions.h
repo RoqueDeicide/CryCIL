3223d3dde6cc9c0e053392e3956ccf2b7473570f
RoqueDeicide <roquegodslayer@hotmail.com>
2015-04-16 17:17:29 +0400

    Finally updated documentation for IMonoExceptions functions.

--- MonoInterface/Interfaces/IMonoExceptions.h
+++ MonoInterface/Interfaces/IMonoExceptions.h
@@ -9,8 +9,6 @@ struct IMonoExceptions
 
 	//! Creates a new Mono exception object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param assembly  Mono assembly where the exception class is defined.
 	//! @param nameSpace Name space where the exception class is defined.
 	//! @param name      Name of the exception class.
@@ -23,8 +21,6 @@ struct IMonoExceptions
 
 	//! Creates a new System.Exception object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
@@ -33,16 +29,12 @@ struct IMonoExceptions
 
 	//! Creates a new System.AppDomainUnloadedException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception AppDomainUnloaded(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ArgumentException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param argumentName Name of invalid argument.
 	//! @param message      Text message to supply with the exception.
@@ -52,16 +44,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception Argument(const char *argumentName, const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ArgumentNullException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception ArgumentNull(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ArgumentOutOfRangeException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -70,16 +58,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception ArgumentOutOfRange(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ArithmeticException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception Arithmetic(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ArrayTypeMismatchException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -88,16 +72,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception ArrayTypeMismatch(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.BadImageFormatException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception BadImageFormat(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.CannotUnloadAppDomainException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -106,8 +86,6 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception CannotUnloadAppDomain(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.DivideByZeroException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
@@ -115,16 +93,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception DivideByZero(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.ExecutionEngineException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception ExecutionEngine(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.IO.FileNotFoundException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param fileName Name of the file that was not found.
 	//! @param message Text message to supply with the exception.
@@ -134,16 +108,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception FileNotFound(const char *fileName, const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.IndexOutOfRangeException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception IndexOutOfRange(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.InvalidCastException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -152,16 +122,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception InvalidCast(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.IO.IOException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception IO(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.MissingMethodException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -170,16 +136,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception MissingMethod(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.MissingMethodException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param class_name  Name of the class where the method was looked up.
 	//! @param member_name Name of missing method.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception MissingMethod(const char *class_name, const char *member_name) = 0;
 	//! Creates a new System.NotImplementedException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -188,16 +150,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception NotImplemented(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.NullReferenceException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception NullReference(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.OverflowException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -206,16 +164,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception Overflow(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.Security.SecurityException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception Security(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.Runtime.Serialization.SerializationException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -224,8 +178,6 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception Serialization(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.StackOverflowException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
@@ -233,16 +185,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception StackOverflow(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.SynchronizationLockException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception SynchronizationLock(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.Threading.ThreadAbortException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -253,16 +201,12 @@ struct IMonoExceptions
 	//!
 	//! @param message Message to supply with the exception object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception ThreadState(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.TypeInitializationException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param type_name Name of the type that wasn't initialized properly.
 	//! @param inner     Optional object that represents an exception that caused this one.
@@ -271,16 +215,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception TypeInitialization(const char *type_name, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.TypeLoadException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception TypeLoad(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.InvalidOperationException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
@@ -289,16 +229,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception InvalidOperation(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.MissingFieldException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception MissingField(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new System.MissingFieldException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param class_name  Name of the class where field was looked up.
 	//! @param member_name Name of the missing field.
@@ -307,16 +243,12 @@ struct IMonoExceptions
 	VIRTUAL_API virtual mono::exception MissingField(const char *class_name, const char *member_name) = 0;
 	//! Creates a new System.NotSupportedException object.
 	//!
-	//! Returned object should be deleted we no longer in use.
-	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
 	//!
 	//! @returns An IMonoException wrapper.
 	VIRTUAL_API virtual mono::exception NotSupported(const char *message = nullptr, mono::exception inner = nullptr) = 0;
 	//! Creates a new CryCil.Engine.CryEngineException object.
-	//!
-	//! Returned object should be deleted we no longer in use.
 	//!
 	//! @param message Text message to supply with the exception.
 	//! @param inner   Optional object that represents an exception that caused this one.
