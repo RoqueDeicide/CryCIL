e7d0c905af9b08483e3efe15a948e121a571d302
RoqueDeicide <roquegodslayer@hotmail.com>
2015-05-27 16:04:29 +0400

    Added simple sphere structure.

---
+++ Cryambly/Mathematics/Geometry/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CryCil.Geometry
+{
+	/// <summary>
+	/// Encapsulates information that describes a very simple sphere.
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential)]
+	public struct Sphere
+	{
+		/// <summary>
+		/// Coordinates of the center of this sphere.
+		/// </summary>
+		public Vector3 Center;
+		/// <summary>
+		/// Radius of this sphere.
+		/// </summary>
+		public float Radius;
+	}
+}
