de7239e6e5ce20e2f024954da1afd8e111d45020
RoqueDeicide <roquegodslayer@hotmail.com>
2015-06-12 19:02:15 +0400

    Added function to TimeUtilities that converts Mono ticks to CryEngine ticks.

--- MonoInterface/TimeUtilities.h
+++ MonoInterface/TimeUtilities.h
@@ -3,12 +3,18 @@
 struct TimeUtilities
 {
 	static long long CryEngineTicksToMonoTicks(long long ticks);
+	static long long MonoTicksToCryEngineTicks(long long ticks);
 	static long long SecondsToMonoTicks(float seconds);
 };
 
 inline long long TimeUtilities::CryEngineTicksToMonoTicks(long long ticks)
 {
 	return ticks * 100L;
+}
+
+inline long long TimeUtilities::MonoTicksToCryEngineTicks(long long ticks)
+{
+	return ticks / 100L;
 }
 
 inline long long TimeUtilities::SecondsToMonoTicks(float seconds)
