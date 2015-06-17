5f76f0190a4a123ab7535f27c31104dad00748eb
RoqueDeicide <roquegodslayer@hotmail.com>
2015-06-18 03:37:39 +0400

    Added DefaultInitializationStages enumeration.

---
+++ Cryambly/RunTime/DefaultInitializationStages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryCil.RunTime
+{
+	/// <summary>
+	/// Enumeration of indices of default initialization stages.
+	/// </summary>
+	public enum DefaultInitializationStages
+	{
+		/// <summary>
+		/// Index of the initialization stage during which entities defined in CryCIL are registered.
+		/// </summary>
+		EntityRegistrationStage = 1000000,
+		/// <summary>
+		/// Index of the initialization stage during which actors defined in CryCIL are registered.
+		/// </summary>
+		ActorsRegistrationStage = 2000000,
+		/// <summary>
+		/// Index of the initialization stage during which game modes defined in CryCIL are registered.
+		/// </summary>
+		GameModeRegistrationStage = 3000000,
+		/// <summary>
+		/// Index of the initialization stage during which data required to register CryCIL flow nodes is
+		/// gathered.
+		/// </summary>
+		FlowNodeRecognitionStage = 4000000
+	}
+}
