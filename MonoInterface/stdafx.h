aa4f5d9429d81def50ea73b173e2e937d0529d0a
RoqueDeicide <roquegodslayer@hotmail.com>
2015-07-06 17:27:28 +0400

    Imported old function that prints out a warning.

--- MonoInterface/stdafx.h
+++ MonoInterface/stdafx.h
@@ -71,3 +71,16 @@
 #endif
 
 // TODO: reference additional headers your program requires here
+
+//////////////////////////////////////////////////////////////////////////
+//! Reports a warning to validator with WARNING severity.
+inline void MonoWarning(const char *format, ...) PRINTF_PARAMS(1, 2);
+inline void MonoWarning(const char *format, ...)
+{
+	if (!format)
+		return;
+	va_list args;
+	va_start(args, format);
+	GetISystem()->WarningV(VALIDATOR_MODULE_GAME, VALIDATOR_WARNING, 0, nullptr, format, args);
+	va_end(args);
+}
