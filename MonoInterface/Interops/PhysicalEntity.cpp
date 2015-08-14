35f67ada949c9b586ecbbcf0ca81aafe1e385c0a
RoqueDeicide <roquegodslayer@hotmail.com>
2015-08-14 18:32:59 +0400

    Rewrote some code in PhysicalEntity.cpp to be more safe.

--- MonoInterface/Interops/PhysicalEntity.cpp
+++ MonoInterface/Interops/PhysicalEntity.cpp
@@ -7,7 +7,7 @@ typedef void(*DisposeParametersFunc)(PhysicsParameters *);
 typedef void(*ConvertToMonoParametersFunc)(pe_params *, PhysicsParameters *);
 
 template<typename ParamsType>
-pe_params *ConvertToParams(PhysicsParameters *parameters)
+pe_params *ParamsToCE(PhysicsParameters *parameters)
 {
 	return reinterpret_cast<ParamsType *>(parameters)->ToParams();
 }
@@ -17,23 +17,34 @@ void DisposeParams(PhysicsParameters *parameters)
 	reinterpret_cast<ParamsType *>(parameters)->Dispose();
 }
 template<typename ParamsType>
-void ConvertParamsToMono(pe_params *pars, PhysicsParameters *parameters)
+void ParamsToMono(pe_params *pars, PhysicsParameters *parameters)
 {
 	reinterpret_cast<ParamsType *>(parameters)->FromParams(pars);
 }
 
-ConvertToNativeParametersFunc paramConverters[EPE_Params::ePE_Params_Count] =
-{
-	ConvertToParams<PhysicsParametersLocation>
-};
-DisposeParametersFunc paramDisposers[EPE_Params::ePE_Params_Count] =
-{
-	DisposeParams<PhysicsParametersLocation>
-};
-ConvertToMonoParametersFunc paramConvertersToMono[EPE_Params::ePE_Params_Count] =
-{
-	ConvertParamsToMono<PhysicsParametersLocation>
-};
+#define DECLARE_PARAMS_PROCESSING_FUNC(name, functionPtrType, functionPtr, typeCount) \
+functionPtrType name(int type)\
+{\
+	static functionPtrType funcs[typeCount];\
+	static bool initialized = false;\
+	\
+	if (!initialized)\
+	{\
+		memset(funcs, 0, sizeof(functionPtrType) * typeCount);\
+		funcs[ePE_params_pos] = functionPtr<PhysicsParametersLocation>;\
+	}\
+	\
+	if (type < 0 || type >= typeCount)\
+	{\
+		return nullptr;\
+	}\
+	\
+	return funcs[type];\
+}
+
+DECLARE_PARAMS_PROCESSING_FUNC(GetParamConverterToCE,   ConvertToNativeParametersFunc, ParamsToCE,    ePE_Params_Count)
+DECLARE_PARAMS_PROCESSING_FUNC(GetParamDisposer,        DisposeParametersFunc,         DisposeParams, ePE_Params_Count)
+DECLARE_PARAMS_PROCESSING_FUNC(GetParamConverterToMono, ConvertToMonoParametersFunc,   ParamsToMono,  ePE_Params_Count)
 
 void PhysicalEntityInterop::OnRunTimeInitialized()
 {
@@ -43,23 +54,35 @@ void PhysicalEntityInterop::OnRunTimeInitialized()
 
 int PhysicalEntityInterop::SetParams(IPhysicalEntity *handle, PhysicsParameters *parameters, bool threadSafe)
 {
+	auto converter = GetParamConverterToCE(parameters->type);
+	if (!converter)
+	{
+		return 0;
+	}
+
 	// Convert CryCIL object to CryEngine one.
-	auto params = paramConverters[parameters->type](parameters);
+	auto params = converter(parameters);
 	int result = handle->SetParams(params, threadSafe);
 	// Delete CryEngine object.
 	delete params;
 	// Dispose the CryCIL object.
-	paramDisposers[parameters->type](parameters);
+	GetParamDisposer(parameters->type)(parameters);
 	return result;
 }
 
 int PhysicalEntityInterop::GetParams(IPhysicalEntity *handle, PhysicsParameters *parameters)
 {
+	auto converter = GetParamConverterToCE(parameters->type);
+	if (!converter)
+	{
+		return 0;
+	}
+
 	// Convert CryCIL object to CryEngine one.
-	auto params = paramConverters[parameters->type](parameters);
+	auto params = converter(parameters);
 	int result = handle->GetParams(params);
 	// Store result in the CryCIL object.
-	paramConvertersToMono[parameters->type](params, parameters);
+	GetParamConverterToMono(parameters->type)(params, parameters);
 	// Delete CryEngine object.
 	delete params;
 	return result;
