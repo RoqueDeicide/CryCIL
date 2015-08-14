1ecadea369997ec59b739603ae0e7b999fa46966
RoqueDeicide <roquegodslayer@hotmail.com>
2015-08-14 18:01:24 +0400

    Added a wrapper for physical geometry.

---
+++ Cryambly/Engine/Physics/PhysicalGeometry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CryCil.Engine.Physics
+{
+	/// <summary>
+	/// Represents .
+	/// </summary>
+	public struct PhysicalGeometry
+	{
+		#region Fields
+		private readonly IntPtr handle;
+		#endregion
+		#region Properties
+		/// <summary>
+		/// Indicates whether this instance is usable.
+		/// </summary>
+		public bool IsValid
+		{
+			get { return this.handle != IntPtr.Zero; }
+		}
+
+		#endregion
+		#region Construction
+		internal PhysicalGeometry(IntPtr handle)
+		{
+			this.handle = handle;
+		}
+		#endregion
+		#region Interface
+
+		#endregion
+		#region Utilities
+		private void AssertInstance()
+		{
+			if (!this.IsValid)
+			{
+				throw new NullReferenceException("This instance is not valid.");
+			}
+		}
+
+
+		#endregion
+		 
+	}
+}
