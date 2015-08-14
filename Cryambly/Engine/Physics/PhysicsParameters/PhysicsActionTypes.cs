d6e4e383953bd7bdc611569e396a04d82313d48a
RoqueDeicide <roquegodslayer@hotmail.com>
2015-08-14 14:10:04 +0400

    Added 4 enumerations that define identifiers of various utility structures used by the physics subsystem.

---
+++ Cryambly/Engine/Physics/PhysicsParameters/PhysicsActionTypes.cs
@@ -0,0 +1,90 @@
+﻿namespace CryCil.Engine.Physics
+{
+	/// <summary>
+	/// Enumeration of types of actions that can be applied to physical entities.
+	/// </summary>
+	public enum PhysicsActionTypes
+	{
+		/// <summary>
+		/// Identifier of the action that moves the physical entity.
+		/// </summary>
+		Move = 1,
+		/// <summary>
+		/// Identifier of the action that applies impulse to the physical entity.
+		/// </summary>
+		Impulse = 2,
+		/// <summary>
+		/// Identifier of the action that drive the physical entity that is a vehicle.
+		/// </summary>
+		Drive = 3,
+		/// <summary>
+		/// Identifier of the action that resets the physical entity.
+		/// </summary>
+		Reset = 4,
+		/// <summary>
+		/// Identifier of the action that adds constraint to the physical entity.
+		/// </summary>
+		AddConstraint = 5,
+		/// <summary>
+		/// Identifier of the action that updates constraint in the physical entity.
+		/// </summary>
+		UpdateConstraint = 6,
+		/// <summary>
+		/// Identifier of the action that applies a fake collision to the physical entity.
+		/// </summary>
+		RegisterCollisionEvent = 7,
+		/// <summary>
+		/// Identifier of the action that awakes the physical entity.
+		/// </summary>
+		Awake = 8,
+		/// <summary>
+		/// Identifier of the action that removes all articulated parts from the physical entity.
+		/// </summary>
+		RemoveAllParts = 9,
+		/// <summary>
+		/// Identifier of the action that sets velocity of the physical entity.
+		/// </summary>
+		SetVelocity = 10,
+		/// <summary>
+		/// Identifier of the action that attaches points to the physical entity that is a soft body.
+		/// </summary>
+		AttachPoints = 11,
+		/// <summary>
+		/// Identifier of the action that attaches the physical entity that is a rope to the vertex of
+		/// another entity.
+		/// </summary>
+		TargetVertex = 12,
+		/// <summary>
+		/// Identifier of the action that resets the transformation matrix of one of the parts of the
+		/// physical entity.
+		/// </summary>
+		ResetPartMatrix = 13,
+		/// <summary>
+		/// Identifier of the action that notifies the physical entity that is a rope that an entity it was
+		/// attached to was moved.
+		/// </summary>
+		Notify = 14,
+		/// <summary>
+		/// Identifier of the action that specifies the conditions for automatic detachment of parts of the
+		/// physical entity that is an articulated body with pre-baked physics simulation.
+		/// </summary>
+		AutoPartDetachment = 15,
+		/// <summary>
+		/// Identifier of the action that that transfers a part from one physical entity to another.
+		/// </summary>
+		MoveParts = 16,
+		/// <summary>
+		/// Identifier of the action that updates transformations of a number of parts of the physical
+		/// entity.
+		/// </summary>
+		BatchPartsUpdate = 17,
+		/// <summary>
+		/// Identifier of the action that slices the physical entity with a plane.
+		/// </summary>
+		Slice = 18,
+		/// <summary>
+		/// Number of types of actions.
+		/// </summary>
+		Count
+	}
+}
---
+++ Cryambly/Engine/Physics/PhysicsParameters/PhysicsStatusTypes.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace CryCil.Engine.Physics
+{
+	/// <summary>
+	/// Enumeration of types of structures that are used to query various information about the physical
+	/// entity.
+	/// </summary>
+	public enum PhysicsStatusTypes
+	{
+		/// <summary>
+		/// Identifier of the structure that is used to query position of the physical entity.
+		/// </summary>
+		Position = 1,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the living physical entity.
+		/// </summary>
+		Living = 2,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that is a
+		/// vehicle.
+		/// </summary>
+		Vehicle = 4,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that is a
+		/// wheel.
+		/// </summary>
+		Wheel = 5,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that is a
+		/// joint.
+		/// </summary>
+		Joint = 6,
+		/// <summary>
+		/// Identifier of the structure that is used to query awakeness of the physical entity.
+		/// </summary>
+		Awake = 7,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of dynamics of the living physical
+		/// entity.
+		/// </summary>
+		Dynamics = 8,
+		/// <summary>
+		/// Deprecated.
+		/// </summary>
+		[Obsolete]
+		Collisions = 9,
+		/// <summary>
+		/// Identifier of the structure that is used to query surface identifier of the physical entity.
+		/// </summary>
+		Id = 10,
+		/// <summary>
+		/// Deprecated.
+		/// </summary>
+		[Obsolete]
+		TimeSlices = 11,
+		/// <summary>
+		/// Identifier of the structure that is used to query number of parts the physical entity currently
+		/// consists of.
+		/// </summary>
+		PartCount = 12,
+		/// <summary>
+		/// Identifier of the structure that is used to check whether physical entity contains the point.
+		/// </summary>
+		ContainsPoint = 13,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that is a rope.
+		/// </summary>
+		Rope = 14,
+		/// <summary>
+		/// Identifier of the structure that is used to query current abilities of the physical entity that
+		/// is a vehicle.
+		/// </summary>
+		VehicleAbilities = 15,
+		/// <summary>
+		/// Identifier of the structure that is used to check whether this physical entity is a
+		/// place-holder.
+		/// </summary>
+		PlaceHolder = 16,
+		/// <summary>
+		/// Identifier of the structure that is used to query positions vertexes of the physical entity
+		/// that is a soft body.
+		/// </summary>
+		SoftVertex = 17,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of sensors that are attached to the
+		/// physical entity.
+		/// </summary>
+		Sensors = 18,
+		/// <summary>
+		/// Identifier of the structure that is used to check whether a point that is projected along a
+		/// specific direction will fall onto convex hull of the physical entity.
+		/// </summary>
+		SampleContactArea = 19,
+		/// <summary>
+		/// Identifier of the structure that is used to check whether physical entity can change the
+		/// orientation that was explicitly set from the outside (by baking into parts/geometries).
+		/// </summary>
+		CanAlterSetOrientation = 20,
+		/// <summary>
+		/// Identifier of the structure that is used to check whether new position of the physical entity
+		/// will cause collisions.
+		/// </summary>
+		CheckStance = 21,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that simulates
+		/// water.
+		/// </summary>
+		Water = 22,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the physical entity that is an
+		/// area.
+		/// </summary>
+		Area = 23,
+		/// <summary>
+		/// Identifier of the structure that is used to query extent(?) of the physical entity.
+		/// </summary>
+		Extent = 24,
+		/// <summary>
+		/// Identifier of the structure that is used to get random position on the physical entity.
+		/// </summary>
+		Random = 25,
+		/// <summary>
+		/// Identifier of the structure that is used to query status of the constraint this physical entity
+		/// has attached to it.
+		/// </summary>
+		Constraint = 26,
+		/// <summary>
+		/// Identifier of the structure that is used to query position of the physical entity in the
+		/// network.
+		/// </summary>
+		NetworkPosition = 27,
+		/// <summary>
+		/// Number of identifiers.
+		/// </summary>
+		Count
+	}
+}
