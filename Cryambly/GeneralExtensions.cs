c82d0b7406556e0e0899e352731ed1054b6f61e8
RoqueDeicide <roquegodslayer@hotmail.com>
2015-08-25 13:00:02 +0400

    Added a clamping extension method.

--- Cryambly/GeneralExtensions.cs
+++ Cryambly/GeneralExtensions.cs
@@ -312,6 +312,41 @@ namespace CryCil
 					return number + "th";
 			}
 		}
+		/// <summary>
+		/// Gets a value that is clamped into specified range.
+		/// </summary>
+		/// <typeparam name="ValType">Type of the value to clamp.</typeparam>
+		/// <param name="instance">A value to clamp.</param>
+		/// <param name="min">     Minimal value in the range to clamp the value into.</param>
+		/// <param name="max">     Maximal value in the range to clamp the value into.</param>
+		/// <returns>
+		/// <para><paramref name="instance"/> if it is within the range;</para>
+		/// <para>
+		/// <paramref name="min"/>, if <paramref name="instance"/> is less then <paramref name="min"/>;
+		/// </para>
+		/// <para>
+		/// <paramref name="max"/>, if <paramref name="instance"/> is greater then <paramref name="max"/>.
+		/// </para>
+		/// </returns>
+		/// <exception cref="ArgumentException">Minimal value must be less then maximal value.</exception>
+		public static ValType GetClamped<ValType>(this ValType instance, ValType min, ValType max)
+			where ValType : IComparable<ValType>
+		{
+			int minToMax = min.CompareTo(max);
+			if (minToMax == 0)
+			{
+				return min;
+			}
+			if (minToMax > 0)
+			{
+				throw new ArgumentException("Minimal value must be less then maximal value.");
+			}
+			if (instance.CompareTo(min) < 0)
+			{
+				return min;
+			}
+			return instance.CompareTo(max) > 0 ? max : instance;
+		}
 	}
 	/// <summary>
 	/// Contains old value of the property that has been changed.
