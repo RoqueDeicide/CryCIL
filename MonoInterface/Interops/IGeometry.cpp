edc2c3ce9cbd4a56e6ce2a8b25e68f83bbbfd9d1
RoqueDeicide <roquegodslayer@hotmail.com>
2015-09-04 05:57:15 +0400

    Implemented GeometryShape functionality on C++ side.

--- MonoInterface/Interops/IGeometry.cpp
+++ MonoInterface/Interops/IGeometry.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "IGeometry.h"
+#include "ForeignData.h"
 
 void IGeometryInterop::OnRunTimeInitialized()
 {
@@ -7,6 +8,33 @@ void IGeometryInterop::OnRunTimeInitialized()
 	REGISTER_METHOD(CreateMeshBv);
 	REGISTER_METHOD(CreateMeshVg);
 	REGISTER_METHOD(CreatePrimitive);
+
+	REGISTER_METHOD(GetGeometryType);
+	REGISTER_METHOD(AddRef);
+	REGISTER_METHOD(Release);
+	REGISTER_METHOD(Lock);
+	REGISTER_METHOD(Unlock);
+	REGISTER_METHOD(GetBBox);
+	REGISTER_METHOD(PointInsideStatus);
+	REGISTER_METHOD(IntersectLocked);
+	REGISTER_METHOD(IntersectLockedDefault);
+	REGISTER_METHOD(FindClosestPointInternal);
+	REGISTER_METHOD(CalcVolumetricPressure);
+	REGISTER_METHOD(CalculateBuoyancyInternal);
+	REGISTER_METHOD(CalculateMediumResistanceInternal);
+	REGISTER_METHOD(IsConvexInternal);
+	REGISTER_METHOD(GetPrimitiveCount);
+	REGISTER_METHOD(GetVolume);
+	REGISTER_METHOD(GetCenter);
+	REGISTER_METHOD(Subtraction);
+	REGISTER_METHOD(GetSubtractionsCount);
+	REGISTER_METHOD(GetMeshUpdatesInternal);
+	REGISTER_METHOD(GetForeignData);
+	REGISTER_METHOD(SetForeignData);
+	REGISTER_METHOD(GetErrorCount);
+	REGISTER_METHOD(BoxifyInternal);
+	REGISTER_METHOD(SanityCheck);
+	REGISTER_METHOD(GetData);
 }
 
 IGeometry *IGeometryInterop::CreateMesh(Vec3* vertices, uint16* indices, byte* materialIds, int* foreignIds,
@@ -39,4 +67,184 @@ IGeometry *IGeometryInterop::CreateMeshVg(Vec3* vertices, uint16* indices, byte*
 IGeometry *IGeometryInterop::CreatePrimitive(int type, primitives::primitive *prim)
 {
 	return gEnv->pPhysicalWorld->GetGeomManager()->CreatePrimitive(type, prim);
+}
+
+int IGeometryInterop::GetGeometryType(IGeometry *handle)
+{
+	return handle->GetType();
+}
+
+int IGeometryInterop::AddRef(IGeometry *handle)
+{
+	return handle->AddRef();
+}
+
+void IGeometryInterop::Release(IGeometry *handle)
+{
+	handle->Release();
+}
+
+void IGeometryInterop::Lock(IGeometry *handle, int bWrite)
+{
+	handle->Lock(bWrite);
+}
+
+void IGeometryInterop::Unlock(IGeometry *handle, int bWrite)
+{
+	handle->Unlock(bWrite);
+}
+
+void IGeometryInterop::GetBBox(IGeometry *handle, primitives::box *pbox)
+{
+	handle->GetBBox(pbox);
+}
+
+int IGeometryInterop::PointInsideStatus(IGeometry *handle, Vec3 *pt)
+{
+	return handle->PointInsideStatus(*pt);
+}
+
+mono::Array IGeometryInterop::IntersectLocked(IGeometry *handle, IGeometry *pCollider, geom_world_data *pdata1,
+											  geom_world_data *pdata2, intersection_params *pparams)
+{
+	WriteLockCond lock;
+
+	geom_contact *contactsPtr;
+	int contactCount = handle->IntersectLocked(pCollider, pdata1, pdata2, pparams, contactsPtr, lock);
+
+	if (contactCount == 0)
+	{
+		return nullptr;
+	}
+
+	IMonoClass *geomContactClass = MonoEnv->Cryambly->GetClass("CryCil.Engine.Physics", "GeometryContact");
+	IMonoArray<geom_contact> contacts = MonoEnv->Objects->Arrays->Create(contactCount, geomContactClass);
+
+	MonoGCHandle gcHandle = MonoEnv->GC->Pin(contacts);
+
+	for (int i = 0; i < contactCount; i++)
+	{
+		contacts[i] = contactsPtr[i];
+	}
+
+	return contacts;
+}
+
+mono::Array IGeometryInterop::IntersectLockedDefault(IGeometry *handle, IGeometry *pCollider)
+{
+	WriteLockCond lock;
+
+	geom_contact *contactsPtr;
+	int contactCount = handle->IntersectLocked(pCollider, nullptr, nullptr, nullptr, contactsPtr, lock);
+
+	if (contactCount == 0)
+	{
+		return nullptr;
+	}
+
+	IMonoClass *geomContactClass = MonoEnv->Cryambly->GetClass("CryCil.Engine.Physics", "GeometryContact");
+	IMonoArray<geom_contact> contacts = MonoEnv->Objects->Arrays->Create(contactCount, geomContactClass);
+
+	MonoGCHandle gcHandle = MonoEnv->GC->Pin(contacts);
+
+	for (int i = 0; i < contactCount; i++)
+	{
+		contacts[i] = contactsPtr[i];
+	}
+
+	return contacts;
+}
+
+int IGeometryInterop::FindClosestPointInternal(IGeometry *handle, geom_world_data *pgwd, int *iPrim, int *iFeature,
+											   Vec3 *ptdst0, Vec3 *ptdst1, Vec3* ptres, int nMaxIters)
+{
+	return handle->FindClosestPoint(pgwd, *iPrim, *iFeature, *ptdst0, *ptdst1, ptres, nMaxIters);
+}
+
+void IGeometryInterop::CalcVolumetricPressure(IGeometry *handle, geom_world_data *gwd, Vec3 *epicenter, float k,
+											  float rmin, Vec3 *P, Ang3 *L)
+{
+	handle->CalcVolumetricPressure(gwd, *epicenter, k, rmin, handle->GetCenter(), *P, *reinterpret_cast<Vec3 *>(L));
+}
+
+float IGeometryInterop::CalculateBuoyancyInternal(IGeometry *handle, primitives::plane *pplane, geom_world_data *pgwd,
+												  Vec3 *submergedMassCenter)
+{
+	return handle->CalculateBuoyancy(pplane, pgwd, *submergedMassCenter);
+}
+
+void IGeometryInterop::CalculateMediumResistanceInternal(IGeometry *handle, primitives::plane *pplane,
+														 geom_world_data *pgwd, Vec3 *dPres, Vec3 *dLres)
+{
+	handle->CalculateMediumResistance(pplane, pgwd, *dPres, *dLres);
+}
+
+int IGeometryInterop::IsConvexInternal(IGeometry *handle, float tolerance)
+{
+	return handle->IsConvex(tolerance);
+}
+
+int IGeometryInterop::GetPrimitiveCount(IGeometry *handle)
+{
+	return handle->GetPrimitiveCount();
+}
+
+float IGeometryInterop::GetVolume(IGeometry *handle)
+{
+	return handle->GetVolume();
+}
+
+Vec3 IGeometryInterop::GetCenter(IGeometry *handle)
+{
+	return handle->GetCenter();
+}
+
+int IGeometryInterop::Subtraction(IGeometry *handle, IGeometry *pGeom, geom_world_data *pdata1, geom_world_data *pdata2,
+								  bool logUpdates)
+{
+	return handle->Subtract(pGeom, pdata1, pdata2, logUpdates ? 1 : 0);
+}
+
+int IGeometryInterop::GetSubtractionsCount(IGeometry *handle)
+{
+	return handle->GetSubtractionsCount();
+}
+
+bop_meshupdate *IGeometryInterop::GetMeshUpdatesInternal(IGeometry *handle)
+{
+	return static_cast<bop_meshupdate *>(handle->GetForeignData(DATA_MESHUPDATE));
+}
+
+ForeignData IGeometryInterop::GetForeignData(IGeometry *handle)
+{
+	ForeignData data;
+	data.id = handle->GetiForeignData();
+	data.handle = handle->GetForeignData(data.id);
+	return data;
+}
+
+void IGeometryInterop::SetForeignData(IGeometry *handle, ForeignData data)
+{
+	handle->SetForeignData(data.handle, data.id);
+}
+
+int IGeometryInterop::GetErrorCount(IGeometry *handle)
+{
+	return handle->GetErrorCount();
+}
+
+int IGeometryInterop::BoxifyInternal(IGeometry *handle, primitives::box *pboxes, int nMaxBoxes,
+									 IGeometry::SBoxificationParams *parameters)
+{
+	return handle->Boxify(pboxes, nMaxBoxes, *parameters);
+}
+
+int IGeometryInterop::SanityCheck(IGeometry *handle)
+{
+	return handle->SanityCheck();
+}
+
+const primitives::primitive *IGeometryInterop::GetData(IGeometry *handle)
+{
+	return handle->GetData();
 }
--- MonoInterface/Interops/IGeometry.h
+++ MonoInterface/Interops/IGeometry.h
@@ -2,6 +2,8 @@
 
 #include "IMonoInterface.h"
 
+struct ForeignData;
+
 struct IGeometryInterop : public IMonoInterop < true, true >
 {
 	virtual const char *GetInteropClassName() override { return "GeometryShape"; }
@@ -17,4 +19,38 @@ struct IGeometryInterop : public IMonoInterop < true, true >
 	static IGeometry *CreateMeshVg(Vec3* vertices, uint16* indices, byte* materialIds, int* foreignIds, int triangleCount,
 								   int flags, SMeshBVParams *vgParams, float approximationTolerance);
 	static IGeometry *CreatePrimitive(int type, primitives::primitive *prim);
+
+	static int GetGeometryType(IGeometry *handle);
+	static int AddRef(IGeometry *handle);
+	static void Release(IGeometry *handle);
+	static void Lock(IGeometry *handle, int bWrite);
+	static void Unlock(IGeometry *handle, int bWrite);
+	static void GetBBox(IGeometry *handle, primitives::box *pbox);
+	static int PointInsideStatus(IGeometry *handle, Vec3 *pt);
+	static mono::Array IntersectLocked(IGeometry *handle, IGeometry *pCollider, geom_world_data *pdata1,
+									   geom_world_data *pdata2, intersection_params *pparams);
+	static mono::Array IntersectLockedDefault(IGeometry *handle, IGeometry *pCollider);
+	static int FindClosestPointInternal(IGeometry *handle, geom_world_data *pgwd, int *iPrim, int *iFeature, Vec3 *ptdst0,
+										Vec3 *ptdst1, Vec3* ptres, int nMaxIters);
+	static void CalcVolumetricPressure(IGeometry *handle, geom_world_data *gwd, Vec3 *epicenter, float k, float rmin,
+									   Vec3 *P, Ang3 *L);
+	static float CalculateBuoyancyInternal(IGeometry *handle, primitives::plane *pplane, geom_world_data *pgwd,
+										   Vec3 *submergedMassCenter);
+	static void CalculateMediumResistanceInternal(IGeometry *handle, primitives::plane *pplane, geom_world_data *pgwd,
+												  Vec3 *dPres, Vec3 *dLres);
+	static int IsConvexInternal(IGeometry *handle, float tolerance);
+	static int GetPrimitiveCount(IGeometry *handle);
+	static float GetVolume(IGeometry *handle);
+	static Vec3 GetCenter(IGeometry *handle);
+	static int Subtraction(IGeometry *handle, IGeometry *pGeom, geom_world_data *pdata1, geom_world_data *pdata2,
+						   bool logUpdates);
+	static int GetSubtractionsCount(IGeometry *handle);
+	static bop_meshupdate *GetMeshUpdatesInternal(IGeometry *handle);
+	static ForeignData GetForeignData(IGeometry *handle);
+	static void SetForeignData(IGeometry *handle, ForeignData data);
+	static int GetErrorCount(IGeometry *handle);
+	static int BoxifyInternal(IGeometry *handle, primitives::box *pboxes, int nMaxBoxes,
+							  IGeometry::SBoxificationParams *parameters);
+	static int SanityCheck(IGeometry *handle);
+	static const primitives::primitive *GetData(IGeometry *handle);
 };
