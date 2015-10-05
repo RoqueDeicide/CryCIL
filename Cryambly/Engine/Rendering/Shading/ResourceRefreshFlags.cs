5cdc8a99082ea910c47f69414d4b9e1e3469884c
RoqueDeicide <roquegodslayer@hotmail.com>
2015-10-05 16:52:14 +0400

    Added enumeration of flags for reloading render resources.

---
+++ Cryambly/Engine/Rendering/Shading/ResourceRefreshFlags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CryCil.Engine.Rendering
+{
+	/// <summary>
+	/// Enumeration of flags that define which render resources to refresh.
+	/// </summary>
+	[Flags]
+	public enum ResourceRefreshFlags
+	{
+		/// <summary>
+		/// When set, specifies that shaders must be reloaded.
+		/// </summary>
+		Shaders = 1,
+		/// <summary>
+		/// When set, specifies that shader textures must be reloaded.
+		/// </summary>
+		ShaderTextures = 2,
+		/// <summary>
+		/// When set, specifies that textures must be reloaded.
+		/// </summary>
+		Textures = 4,
+		/// <summary>
+		/// When set, specifies that geometric objects must be reloaded.
+		/// </summary>
+		Geometry = 8,
+		/// <summary>
+		/// When set, specifies that reloading process must be enforced.
+		/// </summary>
+		ForceReload = 0x10
+	}
+}
---
+++ MonoInterface/TypeMapChecks/ResRefreshFlags.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+
+#include "CheckingBasics.h"
+
+TYPE_MIRROR enum ResRefreshFlags
+{
+	FRO_SHADERS_check = 1,
+	FRO_SHADERTEXTURES_check = 2,
+	FRO_TEXTURES_check = 4,
+	FRO_GEOMETRY_check = 8,
+	FRO_FORCERELOAD_check = 0x10
+};
+
+#define CHECK_ENUM(x) static_assert (ResRefreshFlags::x ## _check == x, "Render resource refresh defines were changed.")
+
+inline void Check()
+{
+	CHECK_ENUM(FRO_SHADERS);
+	CHECK_ENUM(FRO_SHADERTEXTURES);
+	CHECK_ENUM(FRO_TEXTURES);
+	CHECK_ENUM(FRO_GEOMETRY);
+	CHECK_ENUM(FRO_FORCERELOAD);
+}
