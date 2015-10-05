6321925933ede35e7c92b0ce73bda64d7126c063
RoqueDeicide <roquegodslayer@hotmail.com>
2015-10-05 17:01:18 +0400

    Added PositionNormal structure.

---
+++ Cryambly/Mathematics/Geometry/PositionNormal.cs
@@ -0,0 +1,17 @@
+﻿namespace CryCil.Geometry
+{
+	/// <summary>
+	/// Encapsulates coordinates of the point on the surface and a normal to surface at the point.
+	/// </summary>
+	public struct PositionNormal
+	{
+		/// <summary>
+		/// Coordinates of the point.
+		/// </summary>
+		public Vector3 Position;
+		/// <summary>
+		/// Normal to the surface at the point.
+		/// </summary>
+		public Vector3 Normal;
+	}
+}
