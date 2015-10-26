2ada2ea74624183c527e4a2048584e051dd3c16a
RoqueDeicide <roquegodslayer@hotmail.com>
2015-10-26 23:46:45 +0400

    Added a constant that contains the size of pointer type.

--- Cryambly/VariousConstants.cs
+++ Cryambly/VariousConstants.cs
@@ -9,5 +9,15 @@
 		/// Pattern that allows to find characters that are not valid for XML tag names.
 		/// </summary>
 		public const string InvalidXmlCharsPattern = @"[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]";
+		/// <summary>
+		/// The constant that contains the size of pointers in bytes.
+		/// </summary>
+		public const int PointerSize = 
+#if WIN64
+			8
+#else
+			4
+#endif
+						 ;
 	}
 }
