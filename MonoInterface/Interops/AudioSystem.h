8c52c941aa21fe67979cf098fb787665cc18f53b
RoqueDeicide <roquegodslayer@hotmail.com>
2015-12-23 17:46:14 +0400

    Added stub for AudioSystemInterop.

---
+++ MonoInterface/Interops/AudioSystem.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "IMonoInterface.h"
+#include <IAudioSystem.h>
+
+struct AudioSystemInterop : IMonoInterop<true, true>
+{
+	virtual const char *GetInteropClassName() override { return "AudioSystem"; }
+	virtual const char *GetInteropNameSpace() override { return "CryCil."; }
+
+	virtual void OnRunTimeInitialized() override;
+
+	
+};
