80522ab0ae5eebbe138a164f25d4011379aaf178
RoqueDeicide <roquegodslayer@hotmail.com>
2016-02-03 14:32:34 +0400

    Added GeometricEntity and GeometricEntityNode classes to GameLibrary project.

---
+++ GameLibrary/Entities/GeometricEntity.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using CryCil;
+using CryCil.Engine.Data;
+using CryCil.Engine.Logic;
+
+namespace GameLibrary.Entities
+{
+	/// <summary>
+	/// Represents an entity that is a simple geometric object.
+	/// </summary>
+	[Entity("A simple geometric object.", "", "", "prompt.bmp", EntityClassFlags.Default)]
+	public class GeometricEntity : MonoEntity
+	{
+		private string objectPath;
+		/// <summary>
+		/// Gets or sets the path to the file to load geometry from.
+		/// </summary>
+		[EditableProperty(
+			folder:"Geometry",
+			defaultValue:"editor/objects/sphere.cgf",
+			uiControl:EditablePropertyUiControl.Object,
+			description:"Path to the file to load the geometry from.")]
+		public string ObjectModel
+		{
+			get { return this.objectPath; }
+			set
+			{
+				if (this.objectPath == value)
+				{
+					return;
+				}
+
+				if (value.IsNullOrWhiteSpace())
+				{
+					this.objectPath = "";
+					this.Entity.Slots[0].Release();
+					return;
+				}
+
+				this.objectPath = value;
+				this.Entity.Slots[0].LoadStaticObject(this.objectPath);
+			}
+		}
+		/// <summary>
+		/// Initializes base properties of all objects that serve as abstraction layers between CryEngine
+		/// entities and logic defined in CryCIL for them.
+		/// </summary>
+		/// <param name="handle">Pointer to the entity itself.</param>
+		/// <param name="id">    Identifier of the entity.</param>
+		public GeometricEntity(CryEntity handle, EntityId id) : base(handle, id)
+		{
+		}
+		/// <summary>
+		/// When implemented in derived class, releases resources held by this entity.
+		/// </summary>
+		/// <param name="invokedFromNativeCode">
+		/// Indicates whether this entity was released from native code.
+		/// </param>
+		public override void Dispose(bool invokedFromNativeCode)
+		{
+		}
+		/// <summary>
+		/// When implemented in derived class, performs preliminary initialization of this object.
+		/// </summary>
+		public override void Initialize()
+		{
+			this.ResetInEditor += this.Reset;
+		}
+		/// <summary>
+		/// When implemented in derived class, performs final initialization of this object.
+		/// </summary>
+		public override void PostInitialize()
+		{
+			this.Reset(null, false);
+		}
+		/// <summary>
+		/// Synchronizes the state of this entity with its representation in other place (e.g. a save game
+		/// file) .
+		/// </summary>
+		/// <param name="sync">Object that handles synchronization.</param>
+		public override void Synchronize(CrySync sync)
+		{
+		}
+		/// <summary>
+		/// When implemented in derived class updates logical state of this entity.
+		/// </summary>
+		/// <param name="context">The most up-to-date information for this frame.</param>
+		public override void Update(ref EntityUpdateContext context)
+		{
+		}
+		/// <summary>
+		/// When implemented in derived class updates logical state of this entity after most other stuff
+		/// is updated.
+		/// </summary>
+		public override void PostUpdate()
+		{
+		}
+		private void Reset(MonoEntity sender, bool enteringGameMode)
+		{
+			if (!this.ObjectModel.IsNullOrWhiteSpace())
+			{
+				this.Entity.Slots[0].Release();
+				this.Entity.Slots[0].LoadStaticObject(this.objectPath);
+			}
+		}
+	}
+}
---
+++ GameLibrary/FlowNodes/GeometricEntityNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using CryCil.Engine.Logic;
+using GameLibrary.Entities;
+
+namespace GameLibrary.FlowNodes
+{
+	/// <summary>
+	/// Represents a flow node that targets an entity and is configured by it.
+	/// </summary>
+	[FlowNode("entity:GeomEntity",
+		"Can be used to manipulate entities of type GeometricEntity.",
+		FlowNodeFlags.Approved | FlowNodeFlags.TargetEntity | FlowNodeFlags.HideUi)]
+	public class GeometricEntityNode : FlowNode
+	{
+		#region Fields
+		private GeometricEntity target;
+		private readonly OutputPortBool donePort;
+		#endregion
+		#region Properties
+		
+		#endregion
+		#region Construction
+		/// <summary>
+		/// Initializes base properties of the new flow node wrapper object.
+		/// </summary>
+		/// <param name="id">   Identifier of the node.</param>
+		/// <param name="graph">
+		/// Pointer to the native object that represents the FlowGraph where this node is located.
+		/// </param>
+		public GeometricEntityNode(ushort id, IntPtr graph)
+			: base(id, graph)
+		{
+			this.donePort = new OutputPortBool("Done", "Done", "Activates and send boolean value of [true] when " +
+															   "geometry is loaded via LoadGeometry input.");
+		}
+		#endregion
+		#region Interface
+		/// <summary>
+		/// Defines a list of specific properties of this flow node.
+		/// </summary>
+		public override void Define()
+		{
+			this.Inputs = new InputPort[1];
+			this.Inputs[0] = new InputPortString("LoadGeometry", "Load Geometry",
+												 "Specifies the path to the geometric object to " +
+												 "use to represent the target entity.",
+												 this.LoadGeometry, "editor/objects/sphere.cgf");
+
+			this.Outputs = new OutputPort[1];
+			this.Outputs[0] = this.donePort;
+
+			this.EntityTargeted += this.OnEntityTargeted;
+		}
+		#endregion
+		#region Utilities
+		private void LoadGeometry(string s)
+		{
+			if (this.target != null)
+			{
+				this.target.ObjectModel = s;
+
+				this.donePort.Activate(true);
+			}
+		}
+		private void OnEntityTargeted(FlowNode flowNode, EntityId entityId)
+		{
+			if (entityId == default(EntityId))
+			{
+				this.target = null;
+			}
+			else
+			{
+				CryEntity cryEntity = entityId.Entity;
+				this.target = cryEntity.ManagedEntity as GeometricEntity;
+			}
+		}
+		#endregion
+	}
+}
