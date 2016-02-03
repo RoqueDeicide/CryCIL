f2d24d5b3b99de57f40434a7cfe8d32f32c07112
RoqueDeicide <roquegodslayer@hotmail.com>
2016-02-03 14:29:50 +0400

    Replaced Action<> types with proper delegates in FlowNode.Events.cs.

--- Cryambly/Engine/Logic/FlowGraph/FlowNode.Events.cs
+++ Cryambly/Engine/Logic/FlowGraph/FlowNode.Events.cs
@@ -1,30 +1,51 @@
 ﻿using System;
+using System.Linq;
 
 namespace CryCil.Engine.Logic
 {
+	/// <summary>
+	/// Defines a signature of methods that can handle simple events raised by objects that derive from
+	/// <see cref="FlowNode"/>.
+	/// </summary>
+	/// <param name="sender">
+	/// An object of type that derives from <see cref="FlowNode"/> that raised the event.
+	/// </param>
+	public delegate void FlowNodeSimpleEventHandler(FlowNode sender);
+	/// <summary>
+	/// Defines a signature of methods that can handle an event <see cref="FlowNode.EntityTargeted"/>
+	/// raised by objects that derive from <see cref="FlowNode"/>.
+	/// </summary>
+	/// <param name="sender">
+	/// An object of type that derives from <see cref="FlowNode"/> that raised the event.
+	/// </param>
+	/// <param name="id">    
+	/// Identifier of the entity that is now targeted by the <paramref name="sender"/>. If equal to default
+	/// value of <see cref="EntityId"/> then the entity is no longer being targeted.
+	/// </param>
+	public delegate void FlowNodeTargetEventHandler(FlowNode sender, EntityId id);
 	public partial class FlowNode
 	{
 		#region Events
 		/// <summary>
 		/// Occurs when identifier of the entity that is associated with this node changes.
 		/// </summary>
-		public event Action<FlowNode, EntityId> EntityTargeted;
+		public event FlowNodeTargetEventHandler EntityTargeted;
 		/// <summary>
 		/// Occurs when initialization of this node starts.
 		/// </summary>
-		public event Action<FlowNode> Initializing;
+		public event FlowNodeSimpleEventHandler Initializing;
 		/// <summary>
 		/// Occurs when initialization of this node is over.
 		/// </summary>
-		public event Action<FlowNode> Initialized;
+		public event FlowNodeSimpleEventHandler Initialized;
 		/// <summary>
 		/// Occurs when this node is suspended.
 		/// </summary>
-		public event Action<FlowNode> Suspended;
+		public event FlowNodeSimpleEventHandler Suspended;
 		/// <summary>
 		/// Occurs when this node is no longer suspended.
 		/// </summary>
-		public event Action<FlowNode> Resumed;
+		public event FlowNodeSimpleEventHandler Resumed;
 		#endregion
 		#region Raisers
 		/// <summary>
