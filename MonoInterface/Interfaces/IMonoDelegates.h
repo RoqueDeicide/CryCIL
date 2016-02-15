8e2897b3ec99d07dad565f14ddd21a713659257a
RoqueDeicide <roquegodslayer@hotmail.com>
2016-02-15 11:57:02 +0400

    Updated the documentation on IMonoDelegates.

--- MonoInterface/Interfaces/IMonoDelegates.h
+++ MonoInterface/Interfaces/IMonoDelegates.h
@@ -9,24 +9,18 @@ struct IMonoDelegates
 
 	//! Creates a new delegate for a static method.
 	//!
-	//! Wrapper needs to be deleted when not used anymore.
-	//!
 	//! @param delegateType Type that represents the delegate that needs to be created.
 	//! @param name         Name of the delegate type.
 	//! @param method       Method for which the delegate is made.
 	VIRTUAL_API virtual mono::delegat Create(IMonoClass *delegateType, IMonoStaticMethod *method) = 0;
 	//! Creates a new delegate for an instance method.
 	//!
-	//! Wrapper needs to be deleted when not used anymore.
-	//!
 	//! @param delegateType Type that represents the delegate that needs to be created.
 	//! @param method       Method for which the delegate is made.
 	//! @param target       Target of invocation.
-	VIRTUAL_API virtual mono::delegat Create
-		(IMonoClass *delegateType, IMonoMethod *method, mono::object target) = 0;
+	VIRTUAL_API virtual mono::delegat Create(IMonoClass *delegateType, IMonoMethod *method,
+											 mono::object target) = 0;
 	//! Creates a delegate that wraps an unmanaged function pointer.
-	//!
-	//! Wrapper needs to be deleted when not used anymore.
 	//!
 	//! There is a number of restrictions on this function:
 	//! 1) Generics are not supported in interop scenarios.
@@ -40,6 +34,11 @@ struct IMonoDelegates
 	//!
 	//! 5) You cannot use this method to create a delegate from a function pointer to another managed
 	//!    delegate.
+	//!
+	//! 6) The function pointer will be invoked via Platform Invoke mechanism, which means that,
+	//!    if your delegate accepts or returns any managed objects those must be made marshalable.
+	//!    You cannot create delegates from function pointers using most delegate types that are
+	//!    defined in .Net/Mono assemblies since they lack any marshaling information.
 	//!
 	//! @param delegateType    Type of the delegate to use. It needs to match the signature of unmanaged
 	//!                        method.
