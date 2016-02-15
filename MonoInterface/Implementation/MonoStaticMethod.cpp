660684c3f1186090ebfad1c0e4ecad6839444e27
RoqueDeicide <roquegodslayer@hotmail.com>
2016-02-15 11:57:59 +0400

    Added some debug code to MonoStaticMethod.

--- MonoInterface/Implementation/MonoStaticMethod.cpp
+++ MonoInterface/Implementation/MonoStaticMethod.cpp
@@ -2,17 +2,29 @@
 
 #include "MonoStaticMethod.h"
 
+#if 0
+#define StaticMethodMessage CryLogAlways
+#else
+#define StaticMethodMessage(...) void(0)
+#endif
+
 mono::object MonoStaticMethod::Invoke(mono::exception *exc /*= nullptr*/)
 {
+	StaticMethodMessage("Invoking the static method with no parameters.");
+
 	return MonoEnv->Functions->InternalInvoke(this->wrappedMethod, nullptr, nullptr, exc, false);
 }
 
 mono::object MonoStaticMethod::Invoke(IMonoArray<> &params, mono::exception *exc /*= nullptr*/)
 {
+	StaticMethodMessage("Invoking the static method with Mono array parameters.");
+
 	return MonoEnv->Functions->InternalInvokeArray(this->wrappedMethod, nullptr, params, exc, false);
 }
 
 mono::object MonoStaticMethod::Invoke(void **params, mono::exception *exc /*= nullptr*/)
 {
+	StaticMethodMessage("Invoking the static method with simple array parameters.");
+
 	return MonoEnv->Functions->InternalInvoke(this->wrappedMethod, nullptr, params, exc, false);
 }
