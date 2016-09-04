e730158b7628986bf7e74cba9ea10f4eb1270e8b
RoqueDeicide <roquegodslayer@hotmail.com>
2016-09-04 12:53:53 +0400

    Created a stub of the engine module that will handle the CryEngine-Mono interface.

--- CryCil.NativeInterface/CryCil.NativeInterface.cpp
+++ CryCil.NativeInterface/CryCil.NativeInterface.cpp
@@ -3,4 +3,30 @@
 
 #include "stdafx.h"
 
+// Included only once per module.
+#include <CryCore/Platform/platform_impl.inl>
 
+#include <CrySystem/IEngineModule.h>
+#include <CryExtension/ICryFactory.h>
+#include <CryExtension/ClassWeaver.h>
+
+struct CryCil_NativeInterface_EngineModule : public IEngineModule
+{
+	CRYINTERFACE_SIMPLE(IEngineModule);
+	CRYGENERATE_SINGLETONCLASS(CryCil_NativeInterface_EngineModule, "CryCil_NativeInterface_EngineModule",
+							   0x2e173e2036b54640, 0x9cc71fff387c0457);
+	
+	const char *GetName() override { return "NativeInterface"; }
+	const char *GetCategory() override { return "CryCil"; }
+	
+	bool Initialize(SSystemGlobalEnvironment &env, const SSystemInitParams &initParams) override
+	{
+		//
+		// Initialize Mono interface.
+		//
+
+		return true;
+	}
+};
+
+CRYREGISTER_SINGLETON_CLASS(CryCil_NativeInterface_EngineModule)
