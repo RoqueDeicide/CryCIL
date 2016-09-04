ef489dd91e4a334509b5366765b3a02fe395795e
RoqueDeicide <roquegodslayer@hotmail.com>
2016-09-04 12:53:15 +0400

    Cleared up the project stub for CryCil.NativeInterface project.

---
+++ CryCil.NativeInterface/README.md
@@ -0,0 +1,2 @@
+## CryCil.NativeInterface
+This project contains the source code that enables communication between CryEngine C++ code and CryCil managed code.
--- CryCil.NativeInterface/stdafx.h
+++ CryCil.NativeInterface/stdafx.h
@@ -7,10 +7,39 @@
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
-#include <windows.h>
-
-
-
 // TODO: reference additional headers your program requires here
+#include <CryCore/Project/CryModuleDefs.h>
+
+// Insert your headers here
+#include <CryCore/Platform/platform.h>
+
+#include <memory>
+#include <vector>
+#include <map>
+#include <queue>
+#include <string>
+#include <exception>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <CryCore/Project/ProjectDefines.h>
+#include <CryMath/Cry_Math.h>
+#include <CryMath/Cry_Camera.h>
+#include <CrySystem/ISystem.h>
+#include <CryNetwork/INetwork.h>
+#include <CryInput/IInput.h>
+#include <CryScriptSystem/IScriptSystem.h>
+#include <CryEntitySystem/IEntitySystem.h>
+#include <CryNetwork/NetHelpers.h>
+#include <CrySystem/File/ICryPak.h>
+#include <CrySystem/IConsole.h>
+#include <CrySystem/ITimer.h>
+#include <CrySystem/ILog.h>
+#include <CryNetwork/IRemoteControl.h>
+#include <CryNetwork/ISimpleHttpServer.h>
+#include <CryGame/IGameFramework.h>
+#include <CryGame/IGame.h>
+#include <CryFlowGraph/IFlowSystem.h>
+
+#include <CryAudio/IAudioSystem.h>
